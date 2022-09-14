2cd05cb7871ff6fcd8cafa3e43b9898c3c87fc25
Arnold Paye <arnoldpayet@gmail.com>
2022-09-14 07:14:22 -0400

    implementing an observable

---
+++ src/observer_pattern_example/Connection.java
@@ -0,0 +1,9 @@
+package observer_pattern_example;
+
+public class Connection {
+  private String status = "";
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+}
---
+++ src/observer_pattern_example/Main.java
@@ -0,0 +1,14 @@
+package observer_pattern_example;
+
+public class Main {
+  public static void main(String[] args) {
+    Connection sally = new Connection();
+    Connection bob = new Connection();
+    SocialMediaFeed feed = new SocialMediaFeed();
+
+    sally.setStatus("going for a walk");
+    bob.setStatus("eating my lunch");
+
+    feed.printStatuses();
+  }
+}
