c392b3ac46ac75953fed9fe7512c85d05c098a2a
Arnold Paye <arnoldpayet@gmail.com>
2022-09-19 05:36:54 -0400

    challenge the state pattern

---
+++ src/state_pattern_exercise/Main.java
@@ -0,0 +1,14 @@
+package state_pattern_exercise;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Fan fan = new Fan();
+    fan.turnUp();
+    fan.turnUp();
+    fan.turnDown();
+    fan.turnDown();
+    fan.turnUp();
+  }
+
+}
