450fb4fac0bd87f76d725c5ec0097866398d7a3d
Arnold Paye <arnoldpayet@gmail.com>
2022-09-19 05:29:20 -0400

    avoiding large switch statements

---
+++ src/state_pattern_example/Main.java
@@ -0,0 +1,9 @@
+package state_pattern_example;
+
+public class Main {
+  public static void main(String[] args) {
+    MediaPlayer mediaPlayer = new MediaPlayer();
+    mediaPlayer.play();
+    mediaPlayer.pause();
+  }
+}
